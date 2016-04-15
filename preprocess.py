0d256c77e26a73352db26b52c0e0222f587f6f66
JenningsWu <jenningswu@gmail.com>
2016-04-15 15:58:37 -0700

    Add -f command flag for preprocess

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,7 @@ import glob
 import os
 import sys
 from subprocess import call
+import argparse
 
 coreNLPLibDir = "c:\\corenlp"
 trainDir = 'train/corpus1'
@@ -10,11 +11,22 @@ curDir = os.path.abspath(sys.path[0])
 propertiesPath = "properties/StanfordCoreNLP-chinese.properties"
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Pre-process training set.')
+    parser.add_argument('-f', '--force', action='store_true',
+                        help='regenerate existent output files')
+    args = parser.parse_args()
     training = glob.glob(os.path.join(trainDir, "*.txt"))
+    if args.force:
+        fileList = set()
+    else:
+        existFile = glob.glob(os.path.join(curDir, trainTmpDir, "*.json"))
+        fileList = set(map(lambda f: os.path.splitext(os.path.basename(f))[0],
+                           existFile))
     for fName in training:
-        call(['java', '-mx4g', '-cp', os.path.join(coreNLPLibDir, '*'),
-              'edu.stanford.nlp.pipeline.StanfordCoreNLP',
-              '-props', os.path.join(curDir, propertiesPath),
-              '-file', os.path.join(curDir, fName),
-              '-outputFormat', 'json',
-              '-outputDirectory', os.path.join(curDir, trainTmpDir)])
+        if os.path.basename(fName) not in fileList:
+            call(['java', '-mx4g', '-cp', os.path.join(coreNLPLibDir, '*'),
+                  'edu.stanford.nlp.pipeline.StanfordCoreNLP',
+                  '-props', os.path.join(curDir, propertiesPath),
+                  '-file', os.path.join(curDir, fName),
+                  '-outputFormat', 'json',
+                  '-outputDirectory', os.path.join(curDir, trainTmpDir)])
