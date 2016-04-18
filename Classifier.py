b22189cf60b193484519987c0ee90030bc08915b
JenningsWu <jenningswu@gmail.com>
2016-04-18 14:55:34 -0700

    Change code style

--- Classifier.py
+++ Classifier.py
@@ -1,102 +1,105 @@
 # -*- coding: utf-8 -*-
-#Classify partited data
-#sentences description type(desc_type):action(0), speech(1), description(2)
-#Character Personality type(personality):[impulsive(1)/calm(0),Extrovert(1)/Introvert(0),
-#                                         Optimistic(1)/pessimistic(0)]
-import sys
-import os
+# Classify partited data
+# sentences description type(desc_type):action(0), speech(1), description(2)
+# Character Personality type(personality):
+#       [impulsive(1)/calm(0),Extrovert(1)/Introvert(0),
+#        Optimistic(1)/pessimistic(0)]
 import json
 import io
-#Data Structures
-descTypeProb = dict()   #{word:[probability of 3 desc_type]}
-personalityProb = dict()#{word:{desctype:6 probabilities for each personalites},...}
-descType = [0,0,0]#action, speech, description
-descTypeOutput = -1 #action(0), speech(1), description(2)
-personality = [0,0,0,0,0,0]#impulsive,calm, Extrovert, Introvert, Optimistic, pessimistic
-personalityOutput = [0,0,0]#[impulsive(1)/calm(0),Extrovert(1)/Introvert(0),Optimistic(1)/pessimistic(0)]
+# Data Structures
+# {word:[probability of 3 desc_type]}
+descTypeProb = dict()
+# {word:{desctype:6 probabilities for each personalites},...}
+personalityProb = dict()
+descType = [0, 0, 0]  # action, speech, description
+descTypeOutput = -1  # action(0), speech(1), description(2)
+# impulsive,calm, Extrovert, Introvert, Optimistic, pessimistic
+personality = [0, 0, 0, 0, 0, 0]
+# [impulsive(1)/calm(0),Extrovert(1)/Introvert(0),Optimistic(1)/pessimistic(0)]
+personalityOutput = [0, 0, 0]
 
 actionCount = 0
 speechCount = 0
 descriptionCount = 0
 totalCount = 0
 diffWordCount = 0
-modelPath = 'e:/1/k_model.txt'
-testDataPath = 'e:/1/k_partition.txt.json'
-#load model
-modelFile = open(modelPath,'r')
+modelPath = 'k_model.txt'
+testDataPath = 'train/partition/k_partition.txt.json'
+# load model
+modelFile = open(modelPath, 'r')
 diffWordCount = int(modelFile.readline())
-for i in range(0,diffWordCount):
+for i in range(0, diffWordCount):
     line = modelFile.readline()
     data = line.strip('\n').split('\t')
     data[0] = data[0].decode('gb2312')
-    descTypeProb.setdefault(data[0],[0,0,0])
-    for j in range(0,3):
-        descTypeProb[data[0]][j] = data[j+1]
+    descTypeProb.setdefault(data[0], [0, 0, 0])
+    for j in range(0, 3):
+        descTypeProb[data[0]][j] = data[j + 1]
 while True:
     line = modelFile.readline()
     if line:
         data = line.strip('\n').split('\t')
         data[0] = data[0].decode('gb2312')
-        #print data
-        personalityProb.setdefault(data[0],{})
-        for i in range(0,3):
-            personalityProb[data[0]].setdefault(i,[0,0,0,0,0,0])
-            for j in range(0,6):
-                personalityProb[data[0]][i][j] = data[i*6+j+1]
+        # print data
+        personalityProb.setdefault(data[0], {})
+        for i in range(0, 3):
+            personalityProb[data[0]].setdefault(i, [0, 0, 0, 0, 0, 0])
+            for j in range(0, 6):
+                personalityProb[data[0]][i][j] = data[i * 6 + j + 1]
     else:
         break
 modelFile.close()
-#load test data
-test = io.open(testDataPath,'r',encoding = 'utf-8')
-#testData = test.read()
-dataSets = json.load(test,encoding = 'utf-8')
-test.close() 
-#analyze & output:
+# load test data
+test = io.open(testDataPath, 'r', encoding='utf-8')
+# testData = test.read()
+dataSets = json.load(test, encoding='utf-8')
+test.close()
+# analyze & output:
 for dataSet in dataSets:
-    personality = [0,0,0,0,0,0]
+    personality = [0, 0, 0, 0, 0, 0]
     for i in dataSet['chars']:
-        print 'character:',i
+        print 'character:', i.encode('utf-8')
         print '\nsentences:\n'
     for i in dataSet['sentences']:
         sentence = ''
-        descType = [0,0,0]
+        descType = [0, 0, 0]
         for j in i['tokens']:
             word = j['word']
             sentence += word
             try:
-                for k in range(0,3):
+                for k in range(0, 3):
                     descType[k] += float(descTypeProb[word][k])
             except:
                 continue
-        if descType[0]>descType[1] and descType[0] > descType[2]:
+        if descType[0] > descType[1] and descType[0] > descType[2]:
             descTypeOutput = 0
-        elif descType[1]>descType[2]:
+        elif descType[1] > descType[2]:
             descTypeOutput = 1
         else:
             descTypeOutput = 2
-        print sentence
+        print sentence.encode('utf-8')
         if descTypeOutput == 0:
             print "Description Type: Action"
             actionCount += 1
         elif descTypeOutput == 1:
             print "Description Type: Speech"
-            speechCount +=1
+            speechCount += 1
         else:
             print "Description Type: Description"
-            descriptionCount +=1
+            descriptionCount += 1
         for j in i['tokens']:
             word = j['word']
             try:
-                for k in range(0,6):
-                   personality[k] += float(personalityProb[word][descTypeOutput][k])
-
+                for k in range(0, 6):
+                    personality[k] += float(
+                        personalityProb[word][descTypeOutput][k])
             except:
                 continue
-    for k in range(0,3):
-        if personality[2*k] >personality[2*k+1]:
+    for k in range(0, 3):
+        if personality[2 * k] > personality[2 * k + 1]:
             personalityOutput[k] = 1
         else:
-            personalityOutput[k]=0
+            personalityOutput[k] = 0
     print '\nPersonality:\n'
     if personalityOutput[0] == 1:
         print 'impulsive;'
@@ -112,6 +115,6 @@ for dataSet in dataSets:
         print 'pessimistic.'
 totalCount = actionCount + speechCount + descriptionCount
 print'\nWriting Style:\n'
-print'Action:',actionCount/float(totalCount)
-print'Speech:',speechCount/float(totalCount)
-print'Description:',descriptionCount/float(totalCount)
+print'Action:', actionCount / float(totalCount)
+print'Speech:', speechCount / float(totalCount)
+print'Description:', descriptionCount / float(totalCount)
--- Learn.py
+++ Learn.py
@@ -1,24 +1,26 @@
 # -*- coding: gb2312 -*-
-#Learn from the training data
-#Classify partited data
-#sentences description type(desc_type):action(0), speech(1), description(2)
-#Character Personality type(Personality):[impulsive(1)/calm(0),Extrovert(1)/Introvert(0),
-#                                         Optimistic(1)/pessimistic(0)]
+# Learn from the training data
+# Classify partited data
+# sentences description type(desc_type):action(0), speech(1), description(2)
+# Character Personality type(Personality):
+#           [impulsive(1)/calm(0),Extrovert(1)/Introvert(0),
+#            Optimistic(1)/pessimistic(0)]
 import json
 import math
-#Data Structures
-descTypeProb = dict()   #{word:[probability of 3 desc_type]}
-personalityProb = dict()#{word:{desctype:6 probabilities for each personalites},...}
+# Data Structures
+descTypeProb = dict()  # {word:[probability of 3 desc_type]}
+# {word:{desctype:6 probabilities for each personalites},...}
+personalityProb = dict()
 
 wordCount = 0
 learnFilePath = 'train/corpus1/k_train.json'
 modelFilePath = 'k_model.txt'
-#load training data
-learnFile = open(learnFilePath,'r')
+# load training data
+learnFile = open(learnFilePath, 'r')
 learnFileText = learnFile.read()
 dataSets = json.loads(learnFileText.decode('gb2312'))
 learnFile.close()
-#training
+# training
 for dataSet in dataSets:
     personality = dataSet['Personality']
     for i in dataSet['sentences']:
@@ -26,39 +28,41 @@ for dataSet in dataSets:
         for j in i['tokens']:
             word = j['word']
             wordCount += 1
-            descTypeProb.setdefault(word,[0,0,0])
+            descTypeProb.setdefault(word, [0, 0, 0])
             descTypeProb[word][descType] += 1
-            personalityProb.setdefault(word,{})
-            for k in range(0,3):
-                personalityProb[word].setdefault(k,[0,0,0,0,0,0])
-                for p in range(0,3):
+            personalityProb.setdefault(word, {})
+            for k in range(0, 3):
+                personalityProb[word].setdefault(k, [0, 0, 0, 0, 0, 0])
+                for p in range(0, 3):
                     if personality[p] == 1:
-                        personalityProb[word][k][2*p] += 1
+                        personalityProb[word][k][2 * p] += 1
                     else:
-                        personalityProb[word][k][2*p+1] += 1
-#smoothing & modeling
-modelFile = open(modelFilePath,'w')
-modelFile.write(str(len(descTypeProb.keys()))+'\n')
+                        personalityProb[word][k][2 * p + 1] += 1
+# smoothing & modeling
+modelFile = open(modelFilePath, 'w')
+modelFile.write(str(len(descTypeProb.keys())) + '\n')
 
 for word in descTypeProb.keys():
     modelFile.write(word.encode('gb2312') + '\t')
-    for k in range(0,3):
-        descTypeProb[word][k] = (descTypeProb[word][k]+1)/float(3*len(descTypeProb.keys())+wordCount)
-        modelFile.write(str(math.log(descTypeProb[word][k]))+'\t')
-        #print word, descTypeProb[word]
+    for k in range(0, 3):
+        descTypeProb[word][k] = (descTypeProb[word][k] + 1) / \
+            float(3 * len(descTypeProb.keys()) + wordCount)
+        modelFile.write(str(math.log(descTypeProb[word][k])) + '\t')
+        # print word, descTypeProb[word]
     modelFile.write('\n')
 
 for word in personalityProb.keys():
     modelFile.write(word.encode('gb2312') + '\t')
-    for k in range(0,3):
-        for p in range(0,6):
-            personalityProb[word][k][p] = (personalityProb[word][k][p]+1)/float(18*len(personalityProb.keys())+3*wordCount)
-            modelFile.write(str(math.log(personalityProb[word][k][p]))+'\t')
-            #print word, personalityProb[word]
+    for k in range(0, 3):
+        for p in range(0, 6):
+            personalityProb[word][k][p] = (personalityProb[word][k][p] + 1) / \
+                float(18 * len(personalityProb.keys()) + 3 * wordCount)
+            modelFile.write(str(math.log(personalityProb[word][k][p])) + '\t')
+            # print word, personalityProb[word]
     modelFile.write('\n')
 modelFile.close()
 
 
 #    print dataSet.keys()
-#print type(dataSet['sentences'])
-#print dataSet['sentences'][1]['tokens'][1]['word']
+# print type(dataSet['sentences'])
+# print dataSet['sentences'][1]['tokens'][1]['word']
